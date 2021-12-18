870bb8965ae97438cbaff1789c2428b4d154f5fd
Siddh1rt1 <93163996+Siddh1rt1@users.noreply.github.com>
2021-12-18 05:54:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var timeoutID;
 var modus=0;
 var loose=0;
 var timeomat=1000;
+var checkblock=0;
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -15,6 +17,7 @@ function jump(){
         
     },600);
 }
+
 function kauer(){
     if(character.classList == "animatekauer"){return}
     document.getElementById("merkel").src="Game_Data/Merkel_small_A.png";
@@ -31,7 +34,6 @@ function slide(){
     if(character.classList == "animateslide"){return}
     document.getElementById("merkel").src="Game_Data/Merkel_slide_A.png";
 
-    
     character.classList.add("animateslide");
     console.log("slide");
     setTimeout(function(){
@@ -41,35 +43,59 @@ function slide(){
 }
 
 var spawn = setInterval(function() {
-    ticker=Math.floor(Math.random() * 6);
-    timeomat=Math.floor(Math.random()*1000+300);
-    
+    ticker=Math.random();
+    timeomat=Math.floor(Math.random()*1000+800)/1000;
     console.log(timeomat);
+  
 
-if (ticker>=3){
+    if (ticker>0.5){
+        fly.style.animation = "";
+        console.log("blockbeginn");
+        block.style.animation = "block "+timeomat+"s  linear";
+    }
+    else{ 
+        block.style.animation ="";
+        console.log("flybeginn");
+        fly.style.animation = "block "+timeomat+"s  linear";}
 
-    block.style.top="110px";
-    block.style.animation = "block 1s infinite linear";
-   
-}   
-else{    
-    block.style.top="90px";
-    block.style.animation = "block 1s infinite linear";
-    
-   
-    
-}
-},1000);
+},2000);
 
 
+var changeblock = setInterval(function(){
+    if (checkwalk!=1){
+        checkwalk=1
+        document.getElementById("blockimg").src="Game_Data/Block_1.png";
+    }
+    else{
+        checkwalk=0;
+        document.getElementById("blockimg").src="Game_Data/Block_2.png";}
+    },400)
 
 
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20&& characterTop>130){
+
+    let chartop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blocktop=parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+    let charleft=parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    let blockleft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let flyleft=parseInt(window.getComputedStyle(fly).getPropertyValue("left"));
+ 
+    if ((flyleft<10) && (flyleft >-5) && (character.classList !== "animatekauer")) {
+  
+    loose=loose+1
+    document.getElementById("looseSpan").innerHTML = loose
+    counter=0;
+}else{
+    counter++;
+    document.getElementById("scoreSpan").innerHTML = Math.floor(counter/11100);
+};
+   
+
+    if((chartop>=90)&& (charleft-blockleft)>-10){
         
-        loose++;
+        loose=loose+1
         document.getElementById("looseSpan").innerHTML = loose
         counter=0;
         
@@ -79,6 +105,7 @@ var checkDead = setInterval(function() {
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/11100);
     }
 }, 100);
+
 
 var walka = setInterval(function() {
     if(character.classList == "animateslide"){
@@ -90,8 +117,6 @@ var walka = setInterval(function() {
             checkwalk=0;
             document.getElementById("merkel").src="Game_Data/Merkel_slide_B.png";}
             return;
-
-
     }
     
     ;
